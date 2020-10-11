f372f53cee321b5713f1b37a70a0b36ef5428b32
Ben Schofield <benjamin.j.schofield@gmail.com>
2020-10-11 10:34:45 +0100

    Git commit updates and fixes

--- commit-obsidian.sh
+++ commit-obsidian.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 # Git: add and commit changes
 cd /Users/crow/Dropbox/Obsidian-vaults/RavenNest && /usr/local/bin/git add --all && /usr/local/bin/git commit -m "daily crontab backup `date`"
