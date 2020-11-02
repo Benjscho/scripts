2c3811edd2fb91685c6b276b2d53114c0f06aded
Ben Schofield <benjamin.j.schofield@gmail.com>
2020-11-02 10:02:20 +0000

    Add backup to obsidian commit

--- commit-obsidian.sh
+++ commit-obsidian.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # Git: add and commit changes
 cd /Users/crow/Dropbox/Obsidian-vaults/RavenNest && /usr/local/bin/git add --all && /usr/local/bin/git commit -m "daily crontab backup `date`"
+
+# Zip folder and backup
+cd /Users/crow/Dropbox/Obsidian-vaults/ && zip -r RavenNest-backup.zip /Users/crow/Dropbox/Obsidian-vaults/RavenNest
+mv /Users/crow/Dropbox/Obsidian-vaults/RavenNest-backup.zip "/Users/crow/Library/Mobile Documents/com~apple~CloudDocs/Obsidian Backups"
