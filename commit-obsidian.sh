16a0e12d1c0ebbc0a7c35e017697502011113de7
Ben Schofield <benjamin.j.schofield@gmail.com>
2020-11-03 14:47:29 +0000

    overwrite existing backups

--- commit-obsidian.sh
+++ commit-obsidian.sh
@@ -4,4 +4,4 @@ cd /Users/crow/Dropbox/Obsidian-vaults/RavenNest && /usr/local/bin/git add --all
 
 # Zip folder and backup
 cd /Users/crow/Dropbox/Obsidian-vaults/ && zip -r RavenNest-backup.zip /Users/crow/Dropbox/Obsidian-vaults/RavenNest
-mv /Users/crow/Dropbox/Obsidian-vaults/RavenNest-backup.zip "/Users/crow/Library/Mobile Documents/com~apple~CloudDocs/Obsidian Backups"
+mv -f /Users/crow/Dropbox/Obsidian-vaults/RavenNest-backup.zip "/Users/crow/Library/Mobile Documents/com~apple~CloudDocs/Obsidian Backups/RavenNest-backup.zip"
