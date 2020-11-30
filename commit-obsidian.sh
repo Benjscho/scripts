7299fc4d05e48d7389c89320dff5d5715608a838
Ben Schofield <benjamin.j.schofield@gmail.com>
2020-11-30 13:17:31 +0000

    cleanup obsidian commit

--- commit-obsidian.sh
+++ commit-obsidian.sh
@@ -2,7 +2,3 @@
 # Git: add and commit changes
 cd /Users/crow/Dropbox/Obsidian-vaults/RavenNest && /usr/local/bin/git add --all && /usr/local/bin/git commit -m "daily crontab backup `date`"
 
-# Zip folder and backup
-#cd /Users/crow/Dropbox/Obsidian-vaults/ && zip -r RavenNest-backup.zip /Users/crow/Dropbox/Obsidian-vaults/RavenNest
-#rm -f "/Users/crow/Library/Mobile Documents/com~apple~CloudDocs/Obsidian Backups/.RavenNest-backup*.zip.icloud"
-#mv /Users/crow/Dropbox/Obsidian-vaults/RavenNest-backup.zip "/Users/crow/Library/Mobile Documents/com~apple~CloudDocs/Obsidian Backups/RavenNest-backup.zip"
