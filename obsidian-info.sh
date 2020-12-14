87a3d022c343022d4b23e1678af41fea6ae120fb
Ben Schofield <benjamin.j.schofield@gmail.com>
2020-12-14 16:55:22 -0700

    add weeknotes count to obsidian info

--- obsidian-info.sh
+++ obsidian-info.sh
@@ -6,3 +6,6 @@ echo "${WORD} words written"
 NOTES=$(wc *.md | wc | awk '$1 ~ /[0-9]/ {print $1}')
 NOTES=$((NOTES-1))
 echo "${NOTES} total notes"
+WEEKNOTES=$(wc *Weeknotes*.md | wc | awk '$1 ~ /[0-9]/ {print $1}')
+WEEKNOTES=$((WEEKNOTES-1))
+echo "${WEEKNOTES} total Weeknotes"
