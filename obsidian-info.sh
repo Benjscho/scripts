ac7e8f2841c1800d859f123f708c85ce2b43c67a
Ben Schofield <benjamin.j.schofield@gmail.com>
2020-12-19 10:03:52 -0700

    Update style for info

--- obsidian-info.sh
+++ obsidian-info.sh
@@ -2,10 +2,15 @@
 cd /Users/crow/Dropbox/Obsidian-vaults/RavenNest
 
 WORD=$(wc *.md | tail -1 | awk '$1 ~ /[0-9]/ {print $2}')
-echo "${WORD} words written"
+echo -e "Words written: \t\t${WORD}"
+
 NOTES=$(wc *.md | wc | awk '$1 ~ /[0-9]/ {print $1}')
 NOTES=$((NOTES-1))
-echo "${NOTES} total notes"
+echo -e "Total notes: \t\t${NOTES}"
+
+AVGWORDS=$((WORD/NOTES))
+echo -e "Average words per note:\t${AVGWORDS}"
+
 WEEKNOTES=$(wc *Weeknotes*.md | wc | awk '$1 ~ /[0-9]/ {print $1}')
 WEEKNOTES=$((WEEKNOTES-1))
-echo "${WEEKNOTES} total Weeknotes"
+echo -e "Weeknotes: \t\t${WEEKNOTES} "
