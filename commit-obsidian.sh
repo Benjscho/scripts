56e2f3b7e11569a1199e7000260ad2cacc65e8d6
Benjscho <benjamin.j.schofield+coding@gmail.com>
2021-02-21 10:59:36 +0000

    Obsidian commit output to null
    Redirect obsidian commit output to /dev/null so it stops going in my
    stupid mailbox.

--- commit-obsidian.sh
+++ commit-obsidian.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Git: add and commit changes
-cd /Users/crow/Dropbox/Obsidian-vaults/RavenNest && /usr/local/bin/git add --all && /usr/local/bin/git commit -m "daily crontab backup `date`"
+cd /Users/crow/Dropbox/Obsidian-vaults/RavenNest && /usr/local/bin/git add --all && /usr/local/bin/git commit -m "daily crontab backup `date`" &>/dev/null
 
