893c5f5144cf6a06f04cf034a792320c76967edd
Benjscho <benjamin.j.schofield+coding@gmail.com>
2021-07-14 09:57:10 +0100

    Update obsidian folder

--- commit-obsidian.sh
+++ commit-obsidian.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Git: add and commit changes
-cd /Users/crow/Dropbox/Obsidian-vaults/RavenNest && /usr/local/bin/git add --all && /usr/local/bin/git commit -m "daily crontab backup `date`" &>/dev/null
+cd "/Users/crow/Library/Mobile Documents/iCloud~md~obsidian/Documents/RavenNest" && /usr/local/bin/git add --all && /usr/local/bin/git commit -m "daily crontab backup `date`" &>/dev/null
 
